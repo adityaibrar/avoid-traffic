98d4776604c571ffd5c1862db43b368959d8b8c2
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2025-01-13 22:05:44 +0700

    Change: move data to app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import cv2
-from flask import Flask, render_template, Response
+from flask import Flask, render_template,request, Response, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Mengizinkan akses dari domain lain
+
+locations = {
+    "pasar": {"name": "Pasar", "lat": -7.915016, "lng": 113.827289},
+    "mts_2": {"name": "MTS 2", "lat": -7.915497, "lng": 113.816206},
+    "SD": {"name": "SD Dabasah", "lat": -7.913832, "lng": 113.820898},
+    "SMP_1": {"name": "SMPN 1 Bondowoso", "lat": -7.912070, "lng": 113.822498},
+    "SDK": {"name": "SDK", "lat": -7.917106, "lng": 113.822214},
+}
 
 def video_stream():
     # URL streaming HLS
@@ -31,9 +39,42 @@ def video_stream():
 
     cap.release()
 
+@app.route("/calculate_route", methods=["POST"])
+def calculate_route():
+    data = request.json
+    start = data.get("start")
+    end = data.get("end")
+    avoid_traffic = data.get("avoid_traffic", False)
+
+    if not start or not end or start not in locations or end not in locations:
+        return jsonify({"error": "Invalid start or end location"}), 400
+
+    start_location = locations[start]
+    end_location = locations[end]
+
+    if(start_location == end_location):
+        return
+
+    # Tentukan waypoints berdasarkan kondisi
+    waypoints = []
+    if start == "pasar" and end == "mts_2" and avoid_traffic:
+        waypoint1 = {"lat": -7.914000, "lng": 113.820000}
+        waypoints.append(waypoint1)
+    elif start == "pasar" and end == "SMP_1" and avoid_traffic:
+        waypoint2 = {"lat": -7.915000, "lng": 113.820000}
+        waypoints.append(waypoint2)
+
+    # Return rute dan waypoints
+    response = {
+        "start": start_location,
+        "end": end_location,
+        "waypoints": waypoints,
+    }
+    return jsonify(response)
+
 @app.route("/")
 def index():
-    return render_template('index.html')  # File HTML frontend
+    return render_template('index.html', locations= locations)  # File HTML frontend
 
 @app.route("/video_feed")
 def video_feed():
